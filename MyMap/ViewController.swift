77a8a5fcdae418bcfcf5c72e3f0db7e644f04d8f
Kyonosuke Yamashita <bp16103@shibaura-it.ac.jp>
2018-08-15 02:08:42 +0900

    Finish

--- MyMap/ViewController.swift
+++ MyMap/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Text Fieldのdelegate通知先の設定
+        inputText.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,89 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBOutlet weak var dispMap: MKMapView!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // キーボードを閉じる
+        textField.resignFirstResponder()
+        
+        // 入力された文字を取り出す
+        if let searchKey = textField.text {
+            
+            // 入力された値をでバックエリアに表示する
+            print(searchKey)
+            
+            // CLGeocoderインスタンスを取得
+            let geocoder = CLGeocoder()
+            
+            // 入力された文字から位置情報を取得
+            geocoder.geocodeAddressString(searchKey, completionHandler: { (placemarks, error) in
+                
+                // 位置情報が存在する場合はunwarpPlacemarksに取り出す
+                if let unwarpPlacemarks = placemarks {
+                    
+                    // １件めの情報を取り出す
+                    if let firstPlacemark = unwarpPlacemarks.first {
+                        
+                        // 位置情報を取り出す
+                        if let location = firstPlacemark.location {
+                            
+                            // 位置情報から緯度経度をtargetCoordinateに取り出す
+                            let targetCoordinate = location.coordinate
+                            
+                            // 緯度経度をデバックエリアに表示
+                            print(targetCoordinate)
+                            
+                            // MKPointAnnotationインスタンスを取得し、ピンを生成
+                            let pin = MKPointAnnotation()
+                            
+                            // ピンを置く場所に、緯度経度を設定
+                            pin.coordinate = targetCoordinate
+                            
+                            // ピンのタイトルを設定
+                            pin.title = searchKey
+                            
+                            // ピンを地図に置く
+                            self.dispMap.addAnnotation(pin)
+                            
+                            // 経度緯度を中心にして、半径５００mの範囲を表示
+                            self.dispMap.region = MKCoordinateRegionMakeWithDistance(targetCoordinate, 500.0, 500.0)
+                            
+                            
+                        }
+                    }
+                }
+                
+            })
+        }
+        
+        return true
+    }
+    
+    
+    @IBAction func changeMapButtonAction(_ sender: Any) {
+        
+        // mapTypeプロパティー値をトグル
+        
+        // 標準（.standard）→ 航空写真（.satellite） → 航空写真＋標準(.hybrid)
+        
+        // → 3D Flyover(.satelliteFlyover) → 3D Flyover + 標準(.hybridFlyover)
+        if dispMap.mapType == .standard {
+            dispMap.mapType = .satellite
+        } else if dispMap.mapType == .satellite {
+            dispMap.mapType = .hybrid
+        } else if dispMap.mapType == .hybrid {
+            dispMap.mapType = .satelliteFlyover
+        } else if dispMap.mapType == .satelliteFlyover {
+            dispMap.mapType = .hybridFlyover
+        } else {
+            dispMap.mapType = .standard
+        }
+    }
+    
+    
+    
 }
 
